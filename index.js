a9f5bd42cf220e57b159618caaaae5ce4cdea624
DESKTOP-BLYAT\Lauri <1960498+msha@users.noreply.github.com>
2024-03-23 20:52:44 +0200

    add part3 step 5

--- index.js
+++ index.js
@@ -57,6 +57,29 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  const id = Math.floor(Math.random()*1_000_000_000)
+  return id
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
