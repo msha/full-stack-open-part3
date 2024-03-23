bf2cadd18512316677a57f6967289d4d6518492d
DESKTOP-BLYAT\Lauri <1960498+msha@users.noreply.github.com>
2024-03-23 21:26:58 +0200

    app part3 step 8

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express()
 var morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(express.static('dist'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res),
+  ].join(' ')
+})
+)
 
 
 let persons = [
