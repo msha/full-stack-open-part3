f2c9b51a88d2393067023940dab378bafd14d1c4
DESKTOP-BLYAT\Lauri <1960498+msha@users.noreply.github.com>
2024-04-15 03:26:02 +0300

    add part3 step 15

--- index.js
+++ index.js
@@ -72,10 +72,13 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  const id = request.params.id;
+  User.findByIdAndDelete(id)
+    .then(result => {
+      persons = persons.filter(person => person.id !== id)
+      response.status(204).end()
+    })
+    .catch(error => console.log(error))
 })
 
 const generateId = () => {
